720c7305452a5fe3491de60c9f018160490d9171
mike <dinhmq1@gmail.com>
2016-10-29 01:49:09 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
-function getCount() {
-      for (var n = 0, len = document.getElementById("initalNumVal").value; n <= len; n++) {
+var n, output, len, index, x, inputVal;
+function getCount(inputVal) {
+      for (n = 0, len = document.getElementById("initalNumVal").value; n <= len; n++) {
         (function(index) {
           setTimeout(function() {
-        var output = "";
-        if (index % 3 == 0)
-          output += "Fizz";
-        if (index % 5 == 0)
-          output += "Buzz";
-        console.log(output || index);
+          output = "";
+          if (index % 3 == 0)
+            output += "Fizz";
+          if (index % 5 == 0)
+            output += "Buzz";
+
+          x = output || index;
+          inputVal = x;
+          document.getElementById("title1").innerHTML += x + '<br>';
       ;},n * 1000);
     })(n);
   }
 }
+
+function getOutput() {
+  console.log();
+};
